3a52d1ebc243a23fdc578600c37fcf039e9b257c
Kerem Kocatuş <keremkocatus17@gmail.com>
2025-05-16 21:34:18 +0300

    version-1.0.4

--- u2net_model.py
+++ u2net_model.py
@@ -12,30 +12,24 @@ def load_model():
     
     return net
 
-def remove_background(image_path, net):
-    image = Image.open(image_path).convert('RGB')
-    original_size = image.size
-    image_resized = image.resize((320, 320))
+def remove_background(img: Image.Image, net):
+    original_size = img.size
+    image_resized = img.resize((320, 320))
 
-    image_np = np.array(image_resized) / 255.0
-    tmpImg = np.zeros((320, 320, 3))
-    tmpImg[:,:,0] = image_np[:,:,0]
-    tmpImg[:,:,1] = image_np[:,:,1]
-    tmpImg[:,:,2] = image_np[:,:,2]
-    tmpImg = tmpImg.transpose((2, 0, 1))
-    tmpImg = torch.from_numpy(tmpImg).unsqueeze(0).type(torch.FloatTensor)
+    arr = np.array(image_resized, dtype=np.float32) / 255.0          # (320,320,3)
+    tensor = torch.from_numpy(arr.transpose(2,0,1)).unsqueeze(0).float()   # (1,3,320,320)
 
     with torch.no_grad():
-        d1, *_ = net(tmpImg)
+        d1, *_ = net(tensor)
         pred = d1[:,0,:,:]
         pred = (pred - pred.min()) / (pred.max() - pred.min())
         mask = pred.squeeze().cpu().numpy() * 255
         mask = cv2.resize(mask.astype(np.uint8), original_size)
 
-    original = np.array(image)
-    b, g, r = original[:,:,0], original[:,:,1], original[:,:,2]
-    a = mask
-    rgba = np.stack([b, g, r, a], axis=2)
+    original = np.array(img)
+    R, G, B = original[:,:,0], original[:,:,1], original[:,:,2]
+    rgba = np.stack([R, G, B, mask], axis=2)
+    
     result = Image.fromarray(rgba.astype(np.uint8), mode='RGBA')
     
     return result
