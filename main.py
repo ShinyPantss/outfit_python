36e866136ea371a11b5afa501b0adc541bbf906e
Kerem Kocatuş <keremkocatus17@gmail.com>
2025-05-16 21:16:07 +0300

    version-1.0.3

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, File, UploadFile , HTTPException
+from fastapi.responses import StreamingResponse
 from contextlib import asynccontextmanager
-from pydantic import BaseModel
+from PIL import Image
+from io import BytesIO
 from u2net_model import remove_background, load_model
-
-class InputData(BaseModel):
-    model_image: str
 
 net = None
 
@@ -18,10 +17,20 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/process-image")
-async def process_image(input_data: InputData):
-    processed_image = remove_background(input_data.model_image, net)
+async def process_image(file: UploadFile = File(...)):
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(400, "Please send an image")
     
-    return processed_image
+    contents = await file.read()
+    img = Image.open(BytesIO(contents))
+    
+    processed_image = remove_background(img, net)
+    
+    buf = BytesIO()
+    processed_image.save(buf, format="jpg")
+    buf.seek(0)
+    
+    return StreamingResponse(buf, media_type="image/jpg")
 
     
     
